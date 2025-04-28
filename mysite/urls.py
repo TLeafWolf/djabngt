eee8e5879a6741fc5a0767207ae7ebd5e77afb27
Teresa Newman <tnewman9219@gmail.com>
2025-04-28 14:53:31 -0400

    init

--- mysite/urls.py
+++ mysite/urls.py
@@ -16,9 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     #hey redirect all url traffic to the blogs url folder
+    
+    path('account/login', views.LoginView.as_view(), name = 'login'),
+    path('account/logout/', views.LogoutView.as_view(next_page = '/'), name = 'logout'),
     path('', include('blog.urls')),
 ]
